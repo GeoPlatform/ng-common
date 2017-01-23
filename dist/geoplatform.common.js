2f2c41f26aaea879a63ea793ba2def016c4340f4
Patrick Neal <patrickn@imagemattersllc.com>
2017-01-23 14:13:56 -0500

    debugging auth

--- dist/geoplatform.common.js
+++ dist/geoplatform.common.js
@@ -260,6 +260,8 @@
             this.getUser = function(callback) {
                 if(callback && typeof(callback) === 'function') {
 
+                    console.log("Getting user info: " + self.status + ", " + JSON.stringify(_user||{empty:true}));
+
                     //if already checked, return what we have
                     if(self.status === STATUS.INITIALIZED) 
                         callback(_user);
@@ -319,6 +321,7 @@
                 var deferred = $q.defer();
                 
                 if(isDEV()) {
+                    console.log("Dev env");
                     setTimeout(function() {
                         deferred.resolve(_user);
                     }, 1000);
@@ -339,8 +342,11 @@
                             name     : response.first_name[0] + ' ' + response.last_name[0],
                             org      : response.organization[0]
                         });
+                        console.log("Authenticated user: " + JSON.stringify(_user));
+
                     } else {
                         _user = null;       //not authenticated
+                        console.log("Failed to authenticate user");
                     }
 
                     deferred.resolve(_user);
@@ -348,6 +354,9 @@
                 }, function(data, status, headers) {   // failed check
                     console.log("Authentication call failed");
                     deferred.reject(data);
+                }).catch(function(e) {
+                    console.log("Authentication check caught an error: " + e.message);
+                    deferred.reject(e.message);
                 });
 
                 return deferred.promise;
@@ -360,6 +369,8 @@
                 self.status = STATUS.INITIALIZED;
             }, function(err) {
                 self.status = STATUS.INITIALIZED;
+            }).catch(function(e) {
+                console.log("Initial auth check errored");
             });
 
         };
