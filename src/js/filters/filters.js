903c1a7607b8ca00b47bc592e38217d15675867a
Patrick Neal <patrickn@imagemattersllc.com>
2017-07-25 16:13:30 -0400

    added grade filter

--- src/js/filters/filters.js
+++ src/js/filters/filters.js
@@ -96,6 +96,48 @@
         };
     })
 
+
+    .filter('gpReliabilityGrade', function() {
+
+        return function(arg) {
+
+            let o = arg;
+            if(typeof(o) === 'object') {
+                if(o.statistics)        o = o.statistics.reliability || null;
+                else if(o.reliability)  o = o.reliability;
+                else                    o = null;
+            }
+
+            if(!isNaN(o)) {
+
+                o = o*1;
+
+                if(o===null || typeof(o)==='undefined') 
+                    return 'X';
+                else if(o > 90) return 'A';
+                else if(o > 80) return 'B';
+                else if(o > 70) return 'C';
+                else if(o > 60) return 'D';
+                else return 'F';
+
+                // if (value >= 97) letter = 'A+';
+                // else if (value >= 93) letter = 'A';
+                // else if (value >= 90) letter = 'A-';
+                // else if (value >= 87) letter = 'B+';
+                // else if (value >= 83) letter = 'B';
+                // else if (value >= 80) letter = 'B-';
+                // else if (value >= 77) letter = 'C+';
+                // else if (value >= 73) letter = 'C';
+                // else if (value >= 70) letter = 'C-';
+                // else if (value >= 67) letter = 'D+';
+                // else if (value >= 63) letter = 'D';
+                // else if (value >= 60) letter = 'D-';
+            }
+
+            return "X";
+        };
+    })
+
     ;
 
 })(jQuery, angular);
