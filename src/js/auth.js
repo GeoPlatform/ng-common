e2b2ffd6c513033093446a9a00a797d892ec16f1
Gate J <gjantara@gmail.com>
2017-10-09 11:45:08 -0400

    Fix call to "atob"

--- src/js/auth.js
+++ src/js/auth.js
@@ -275,7 +275,7 @@
           if (token) {
             var base64Url = token.split('.')[1];
             var base64 = base64Url.replace('-', '+').replace('_', '/');
-            parsed = JSON.parse($window.atob(base64));
+            parsed = JSON.parse(atob(base64));
           }
           return parsed;
         };
