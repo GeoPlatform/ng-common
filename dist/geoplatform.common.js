8ae0fb07bf251f6251342dfec8df6be7eb6e6833
Gate J <gjantara@gmail.com>
2017-10-24 15:59:04 -0400

    Add dist files

--- dist/geoplatform.common.js
+++ dist/geoplatform.common.js
@@ -53,293 +53,310 @@
      */
     .service('AuthenticationService', ['$q', '$http', '$location', '$rootScope', '$window', 'GPConfig',
       function($q, $http, $location, $rootScope, $window, Config) {
-       
-      console.log("Loading auth service", Config);
-      //extend Storage prototype
-      Storage.prototype.setObject = function(key, value) {
-        this.setItem(key, btoa(JSON.stringify(value)));
-      };
 
-      Storage.prototype.getObject = function(key) {
-        var value = atob(this.getItem(key));
-        return value && JSON.parse(value);
-      };
+        //extend Storage prototype
+        Storage.prototype.setObject = function(key, value) {
+          this.setItem(key, btoa(JSON.stringify(value)));
+        };
 
-      function User(opts) {
-        for (var p in opts) {
-          this[p] = opts[p];
-        }
-        if (!this.id && this.username)
-          this.id = this.username;
+        Storage.prototype.getObject = function(key) {
+          var value = atob(this.getItem(key));
+          return value && JSON.parse(value);
+        };
 
-        this.toJSON = function() {
-          return {
-            id: this.id,
-            username: this.username,
-            name: this.name,
-            email: this.email,
-            org: this.org,
-            exp: this.exp
+        function User(opts) {
+          for (var p in opts) {
+            this[p] = opts[p];
+          }
+          if (!this.id && this.username)
+            this.id = this.username;
+
+          this.toJSON = function() {
+            return {
+              id: this.id,
+              username: this.username,
+              name: this.name,
+              email: this.email,
+              org: this.org,
+              exp: this.exp
+            };
           };
-        };
-        this.clone = function() {
-          return new User(this.toJSON());
-        };
-        this.compare = function(arg) {
-          if (arg instanceof User) {
-            return this.id === arg.id;
-          } else if (typeof(arg) === 'object') {
-            return typeof(arg.id) !== 'undefined' &&
-              arg.id === this.id;
-          }
-          return false;
-        };
-      }
 
-      var _user = null;
-
-      var STATUS = {
-        NONE: 0,
-        INITIALIZING: -1, //auth check in progress
-        INITIALIZED: 1 //auth check completed
-      };
-
-
-      /**
-       * Authentication Service
-       */
-      var Service = function() {
-
-        var self = this;
-
-
-        this.status = STATUS.NONE;
-
-        //$q version of getUser
-        this.getUserQ = function() {
-          var deferred = $q.defer();
-          this.getUser(function(user) {
-            deferred.resolve(user);
-          });
-          return deferred.promise;
-        };
-
-        /**
-         * If the callback parameter is specified, this method
-         * will return undefined. Otherwise, it returns the user (or null).
-         *
-         * @param callback optional function to invoke with the user
-         * @return object representing current user
-         */
-        this.getUser = function(callback) {
-          if (callback && typeof(callback) === 'function') {
-
-            // console.log("Getting user info: " + self.status + ", " + JSON.stringify(_user||{empty:true}));
-
-            //if already checked, return what we have
-            if (self.status === STATUS.INITIALIZED)
-              callback(_user);
-            //if not in process of checking, check
-            else if (self.status !== STATUS.INITIALIZING) {
-              //if hasn't been checked for auth yet, do that now
-              self.check().then(function(_user) {
-                self.status = STATUS.INITIALIZED;
-                callback(_user);
-              }, function() {
-                self.status = STATUS.INITIALIZED;
-                callback(null);
-              });
+          this.clone = function() {
+            return new User(this.toJSON());
+          };
+          this.compare = function(arg) {
+            if (arg instanceof User) {
+              return this.id === arg.id;
+            } else if (typeof(arg) === 'object') {
+              return typeof(arg.id) !== 'undefined' &&
+                arg.id === this.id;
             }
-            //if in process of checking, wait until it finishes
-            else {
-              setTimeout(function() {
-                self.getUser(callback);
-              }, 500);
-            }
-            return;
-          }
-          return _user;
+            return false;
+          };
+        }
+
+        var _user = null;
+
+        var STATUS = {
+          NONE: 0,
+          INITIALIZING: -1, //auth check in progress
+          INITIALIZED: 1 //auth check completed
         };
 
-        /**
-         * Redirects the page to the login site
-         */
-        this.login = function() {
-
-          //use current window
-          var current = window.location.href;
-          var redirect = (Config.CALLBACK) ? Config.CALLBACK : current;
-
-          if(Config.AUTH_TYPE !== 'grant' && Config.AUTH_TYPE !== 'token' )
-          {
-             //fail this request
-             throw new Error("Invalid authentication request type.  Must be 'token' or 'grant'.");
-           }
-
-          //check auth type to set login URL: Implicit -> to IDP, Grant -> Resource Provider Login URL
-          var loginUrl = (Config.AUTH_TYPE === 'token') ? Config.IDP_BASE_URL +
-            '/auth/authorize?client_id=' + Config.APP_ID + '&response_type=' +
-            Config.AUTH_TYPE + '&redirect_uri=' + encodeURIComponent(redirect) : Config.LOGIN_URL;
-
-          window.location = loginUrl;
-
-          //This could be writen to use an modal / pop-up for login so you don't have to lose your current page
-          //Logout already executes using a background call
-        };
 
         /**
-         * Performs background logout and requests jwt revokation
+         * Authentication Service
          */
-        this.logout = function() {
-          //implicitly remove incase the idp is down and the revoke call does not work
-          delete window.localStorage.sub;
-          delete window.localStorage.at;
-          _user = null;
-          var url = Config.IDP_BASE_URL + '/auth/revoke';
-          $http.get(url)
-            .then(function(response) {
-              self.removeAuth();
-              self.forceLogin();
-            }, function(err) {
-              console.log(err);
+        var Service = function() {
+
+          var self = this;
+
+
+          this.status = STATUS.NONE;
+
+          //$q version of getUser
+          this.getUserQ = function() {
+            var deferred = $q.defer();
+            this.getUser(function(user) {
+              deferred.resolve(user);
             });
-        };
+            return deferred.promise;
+          };
 
-        /**
-         * Optional force redirect for non-public services
-         */
-        this.forceLogin = function(){
-          if(Config.FORCE_LOGIN && Config.LOGIN_URL){
-            window.location = Config.LOGIN_URL;
-          }
-          return;
-        };
+          /**
+           * If the callback parameter is specified, this method
+           * will return undefined. Otherwise, it returns the user (or null).
+           *
+           * @param callback optional function to invoke with the user
+           * @return object representing current user
+           */
+          this.getUser = function(callback) {
+            if (callback && typeof(callback) === 'function') {
 
-        /**
-         * Get protected user profile
-         */
-        this.getOauthProfile = function() {
-          var Q = $q.defer();
-          //check to make sure we can make called
-          if (window.localStorage.at && window.localStorage.sub) {
-            var url = Config.IDP_BASE_URL + '/api/profile';
+              // console.log("Getting user info: " + self.status + ", " + JSON.stringify(_user||{empty:true}));
+
+              //if already checked, return what we have
+              if (self.status === STATUS.INITIALIZED)
+                //if we have a user and it is not expired return that user to the callback
+                if (_user && _user.exp * 1000 > Date.now()) {
+                  callback(_user);
+                } else if (Config.FORCE_LOGIN) {
+                //force login
+                self.forceLogin();
+              } else {
+                //return null user
+                callback(_user);
+              }
+              //if not in process of checking, check
+              else if (self.status !== STATUS.INITIALIZING) {
+                //if hasn't been checked for auth yet, do that now
+                self.check().then(function(_user) {
+                  self.status = STATUS.INITIALIZED;
+                  callback(_user);
+                }, function() {
+                  self.status = STATUS.INITIALIZED;
+                  callback(null);
+                });
+              }
+              //if in process of checking, wait until it finishes
+              else {
+                setTimeout(function() {
+                  self.getUser(callback);
+                }, 500);
+              }
+              return;
+            }
+            //callback is not a function, chk for force login if user is null and redirect if necessary
+            // if(_user && _user.expired() && Config.FORCE_LOGIN || !_user && Config.FORCE_LOGIN){
+            //   self.forceLogin();
+            // }else{
+            return _user; //return null user
+            // }
+          };
+
+          /**
+           * Redirects the page to the login site
+           */
+          this.login = function() {
+
+            //use current window
+            var current = window.location.href;
+            var redirect = (Config.CALLBACK) ? Config.CALLBACK : current;
+
+            if (Config.AUTH_TYPE !== 'grant' && Config.AUTH_TYPE !== 'token') {
+              //fail this request
+              throw new Error("Invalid authentication request type.  Must be 'token' or 'grant'.");
+            }
+
+            //check auth type to set login URL: Implicit -> to IDP, Grant -> Resource Provider Login URL
+            var loginUrl = (Config.AUTH_TYPE === 'token') ? Config.IDP_BASE_URL +
+              '/auth/authorize?client_id=' + Config.APP_ID + '&response_type=' +
+              Config.AUTH_TYPE + '&redirect_uri=' + encodeURIComponent(redirect) : Config.LOGIN_URL;
+
+            window.location = loginUrl;
+
+            //This could be writen to use an modal / pop-up for login so you don't have to lose your current page
+            //Logout already executes using a background call
+          };
+
+          /**
+           * Performs background logout and requests jwt revokation
+           */
+          this.logout = function() {
+            //implicitly remove incase the idp is down and the revoke call does not work
+            delete window.localStorage.sub;
+            delete window.localStorage.at;
+            _user = null;
+            var url = Config.IDP_BASE_URL + '/auth/revoke';
             $http.get(url)
               .then(function(response) {
-                //when we get it, save it so we don't have to hit the IDP so many times
-                Q.resolve(response.data);
+                self.removeAuth();
+                //goto logout page
+                if (Config.LOGOUT_URL) {
+                  Config.FORCE_LOGIN = false;
+                  window.location = Config.LOGOUT_URL;
+                } else {
+                  window.location = Config.PORTAL_URL || "https://www.geoplatform.gov";
+                }
               }, function(err) {
                 console.log(err);
-                Q.reject(err);
               });
-          }
-          return Q.promise;
+          };
+
+          /**
+           * Optional force redirect for non-public services
+           */
+          this.forceLogin = function() {
+            this.login();
+          };
+
+          /**
+           * Get protected user profile
+           */
+          this.getOauthProfile = function() {
+            var Q = $q.defer();
+            //check to make sure we can make called
+            if (window.localStorage.at && window.localStorage.sub) {
+              var url = Config.IDP_BASE_URL + '/api/profile';
+              $http.get(url)
+                .then(function(response) {
+                  //when we get it, save it so we don't have to hit the IDP so many times
+                  Q.resolve(response.data);
+                }, function(err) {
+                  console.log(err);
+                  Q.reject(err);
+                });
+            }
+            return Q.promise;
+          };
+
+          /**
+           * Queries oauth if the current session is authenticated
+           * @return promise - then(function(user) {}, function(response){})
+           */
+          this.check = function() {
+
+            var deferred = $q.defer();
+
+            self.status = STATUS.INITIALIZING;
+
+            var regex = new RegExp('access_token=.*type=Bearer', 'g'),
+              hashParams = $location.hash(),
+              accessToken,
+              current = ($window && $window.location && $window.location.hash) ? $window.location.hash : $location.url(),
+              getParams = current.match(regex);
+
+            if (getParams) {
+              var tokenString = getParams[0];
+              accessToken = tokenString.split('&')[0]
+                .split('=')[1];
+            }
+
+            var user = self.parseJwt(accessToken);
+            _user = (user) ?
+              new User({
+                id: user.sub,
+                username: user.username,
+                email: user.email,
+                name: user.name,
+                org: user.org,
+                exp: user.exp
+              }) :
+              null;
+
+            if (_user) {
+              self.setAuth(_user, accessToken);
+            } else if (window.localStorage.sub && window.localStorage.at && self.validateJwt(window.localStorage.at)) {
+              self.setAuth(window.localStorage.getObject('sub'), window.localStorage.at);
+            } else {
+              self.removeAuth();
+            }
+
+            //clean hosturl on redirect from oauth
+            if (regex.test(current)) {
+              var cleanUrl = current.replace(regex, '');
+              $window.location = cleanUrl;
+            }
+
+            deferred.resolve(_user);
+            return deferred.promise;
+
+          };
+
+          //=====================================================
+
+          //unpack jwt
+          this.parseJwt = function(token) {
+            var parsed;
+            if (token) {
+              var base64Url = token.split('.')[1];
+              var base64 = base64Url.replace('-', '+').replace('_', '/');
+              parsed = JSON.parse(atob(base64));
+            }
+            return parsed;
+          };
+
+          this.validateJwt = function(token) {
+            var parsed = self.parseJwt(token);
+            var valid = (parsed && parsed.exp && parsed.exp * 1000 > Date.now()) ? true : false;
+            //TODO: chk jwt signature too
+            return valid;
+          };
+
+          this.setAuth = function(user, accessToken) {
+            _user = user;
+            window.localStorage.setObject('sub', user);
+            window.localStorage.at = accessToken;
+            $http.defaults.headers.common.Authorization = 'Bearer ' + accessToken;
+            $http.defaults.useXDomain = true;
+            return;
+          };
+
+          this.removeAuth = function() {
+            delete window.localStorage.sub;
+            delete window.localStorage.at;
+            _user = null;
+            $http.defaults.useXDomain = false;
+            delete $http.defaults.headers.common.Authorization;
+            return;
+          };
+
+          //initialize with auth check
+          this.check().then(function(user) {
+            self.status = STATUS.INITIALIZED;
+          }, function(err) {
+            self.status = STATUS.INITIALIZED;
+          }).catch(function(e) {
+            // console.log("Initial auth check errored");
+          });
+
         };
 
-        /**
-         * Queries oauth if the current session is authenticated
-         * @return promise - then(function(user) {}, function(response){})
-         */
-        this.check = function() {
+        return new Service();
 
-          var deferred = $q.defer();
-
-          self.status = STATUS.INITIALIZING;
-
-          var regex = new RegExp('access_token=.*type=Bearer', 'g'),
-            hashParams = $location.hash(),
-            accessToken,
-            current = ($window && $window.location && $window.location.hash)? $window.location.hash : $location.url(),
-            getParams = current.match(regex);
-
-          if (getParams) {
-            var tokenString = getParams[0];
-            accessToken = tokenString.split('&')[0]
-              .split('=')[1];
-          }
-
-          var user = self.parseJwt(accessToken);
-          _user = (user) ?
-            new User({
-              id: user.sub,
-              username: user.username,
-              email: user.email,
-              name: user.name,
-              org: user.org,
-              exp: user.exp
-            }) :
-            null;
-
-          if (_user) {
-            self.setAuth(_user, accessToken);
-          } else if (window.localStorage.sub && window.localStorage.at && self.validateJwt(window.localStorage.at)) {
-            self.setAuth(window.localStorage.getObject('sub'), window.localStorage.at);
-          } else {
-            self.removeAuth();
-          }
-
-          //clean hosturl on redirect from oauth
-          if (regex.test(current)) {
-            var cleanUrl = current.replace(regex, '');
-            $window.location = cleanUrl;
-          }
-
-          deferred.resolve(_user);
-          return deferred.promise;
-
-        };
-
-        //=====================================================
-
-        //unpack jwt
-        this.parseJwt = function(token) {
-          var parsed;
-          if (token) {
-            var base64Url = token.split('.')[1];
-            var base64 = base64Url.replace('-', '+').replace('_', '/');
-            parsed = JSON.parse(atob(base64));
-          }
-          return parsed;
-        };
-
-        this.validateJwt = function(token) {
-          var parsed = self.parseJwt(token);
-          var valid = (parsed && parsed.exp && parsed.exp * 1000 > Date.now()) ? true : false;
-          //TODO: chk jwt signature too
-          return valid;
-        };
-
-        this.setAuth = function(user, accessToken){
-          _user = user;
-          window.localStorage.setObject('sub', user);
-          window.localStorage.at = accessToken;
-          $http.defaults.headers.common.Authorization = 'Bearer ' + accessToken;
-          $http.defaults.useXDomain = true;
-          return;
-        };
-
-        this.removeAuth = function(){
-          delete window.localStorage.sub;
-          delete window.localStorage.at;
-          _user = null;
-          $http.defaults.useXDomain = false;
-          delete $http.defaults.headers.common.Authorization;
-          return;
-        };
-
-        //initialize with auth check
-        this.check().then(function(user) {
-          self.status = STATUS.INITIALIZED;
-        }, function(err) {
-          self.status = STATUS.INITIALIZED;
-        }).catch(function(e) {
-          // console.log("Initial auth check errored");
-        });
-
-      };
-
-      return new Service();
-
-    }])
+      }
+    ])
 
     .directive('gpLoginButton', ['$timeout', 'AuthenticationService', 'GPConfig',
       function($timeout, AuthenticationService, Config) {
