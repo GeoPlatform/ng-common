186745a7d1a911e480fff1ff4d565cdbd3b07f9a
Patrick Neal <patrickn@imagemattersllc.com>
2018-02-26 12:07:43 -0500

    update created by filter to use auth component, fix type on 'only mine'

--- src/js/query/createdBy.filter.js
+++ src/js/query/createdBy.filter.js
@@ -3,56 +3,70 @@
 
     'use strict';
 
+    class CreatedBy extends GeoPlatform.AuthenticatedComponent {
+
+        constructor($rootScope, AuthenticationService) {
+            super($rootScope, AuthenticationService);
+        }
+
+        $onInit () {
+            super.$onInit();
+            this.value       = null;    //input from user for "created by" value
+            this.collapse    = true;    //hide show controls
+            this.limitToUser = false;   //filter using current user
+            this.modelOptions = {
+                'updateOn': 'default blur',
+                'debounce': {
+                  'default': 250,
+                  'blur': 0
+                }
+            };
+        }
+
+        $onDestroy () {
+            super.$onDestroy();
+            this.value       = null;
+            this.collapse    = null;
+            this.limitToUser = null;
+            this.username    = null;
+        }
+
+        onAuthEvent(event, user) {
+            super.onAuthEvent(event, user);
+            //if user logged out but was still constrained to their items,
+            // refresh without that filter
+            if(!user && this.limitToUser) {
+                this.limitToUser = null;
+                this.filter();
+            }
+        }
+
+        filter () {
+            let value = (this.limitToUser) ? this.authState.user.username : this.value;
+            if(typeof(value) !== 'undefined' && value !== null && value.trim().length === 0)
+                value = null;    //don't accept empty strings
+            this.service.setCreatedBy(value, true);
+        }
+
+        toggleLimitToUser () {
+            this.limitToUser = !this.limitToUser;
+            this.filter();
+        }
+
+        clear () {
+            this.value = null;
+            this.filter();
+        }
+
+    }
+
     angular.module('gp-common').component('createdByFilter', {
 
         bindings: {
             service: '<'   //service filtering by
         },
 
-        controller: function(AuthenticationService) {
-
-            this.$onInit = function() {
-                this.value       = null;    //input from user for "created by" value
-                this.collapse    = true;    //hide show controls
-                this.limitToUser = false;   //filter using current user
-                this.modelOptions = {
-                    'updateOn': 'default blur',
-                    'debounce': {
-                      'default': 250,
-                      'blur': 0
-                    }
-                };
-
-                AuthenticationService.getUserQ().then( user => {
-                    this.username = user ? user.username : null;
-                });
-            };
-
-            this.$onDestroy = function() {
-                this.value       = null;
-                this.collapse    = null;
-                this.limitToUser = null;
-                this.username    = null;
-            };
-
-            this.filter = function() {
-                let value = (this.limitToUser) ? this.username : this.value;
-                if(typeof(value) !== 'undefined' && value !== null && value.trim().length === 0)
-                    value = null;    //don't accept empty strings
-                this.service.setCreatedBy(value, true);
-            };
-
-            this.toggleLimitToUser = function() {
-                this.limitToUser = !this.limitToUser;
-                this.filter();
-            };
-
-            this.clear = function() {
-                this.value = null;
-                this.filter();
-            };
-
-        },
+        controller: CreatedBy,
         template:
         `
             <div class="card c-query-filter">
@@ -78,10 +92,10 @@
                             <span class="glyphicon glyphicon-remove" title="Clear author"
                                 ng-if="$ctrl.value.length&&!$ctrl.limitToUser" ng-click="$ctrl.clear()"></span>
                         </div>
-                    
-                        <label class="control-label u-text--sm text-muted u-pd-top--sm" ng-if="$ctrl.username">
+
+                        <label class="control-label u-text--sm text-muted u-pd-top--sm" ng-if="$ctrl.isAuthenticated()">
                             <input type="checkbox" ng-model="$ctrl.limitToUser" ng-change="$ctrl.filter()">
-                            Only show my maps
+                            Only show my items
                         </label>
 
                     </div>
