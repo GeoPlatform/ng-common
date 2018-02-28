3f90bdc7bd877c04b5fd9acd9a9240891acfe27b
Patrick Neal <patrickn@imagemattersllc.com>
2018-02-28 09:49:39 -0500

    made authcomp into UMD

--- src/js/components/auth.js
+++ src/js/components/auth.js
@@ -1,5 +1,27 @@
 
-(function(GeoPlatform) {
+
+(function (root, factory) {
+    if(typeof define === "function" && define.amd) {
+        // Now we're wrapping the factory and assigning the return
+        // value to the root (window) and returning it as well to
+        // the AMD loader.
+        define([], function() {
+            return (root.AuthenticatedComponent = factory());
+        });
+    } else if(typeof module === "object" && module.exports) {
+        // I've not encountered a need for this yet, since I haven't
+        // run into a scenario where plain modules depend on CommonJS
+        // *and* I happen to be loading in a CJS browser environment
+        // but I'm including it for the sake of being thorough
+        module.exports = (
+            root.AuthenticatedComponent = factory()
+        );
+    } else {
+        GeoPlatform.AuthenticatedComponent = factory();
+    }
+}(this||window, function() {
+
+
 
     const EDIT_ROLE = 'gp_editor';
 
@@ -60,6 +82,7 @@
             return item.createdBy && item.createdBy === this.authState.user.username;
         }
     }
-    GeoPlatform.AuthenticatedComponent = AuthenticatedComponent;
 
-})(GeoPlatform);
+    return AuthenticatedComponent;
+
+}));
